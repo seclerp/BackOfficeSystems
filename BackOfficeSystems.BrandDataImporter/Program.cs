f6a0b35b39b496e7d2b90c56a9bef5e3d3077c9c
Andrii Rublov <andrii.rublov@jet.computer>
2020-04-17 20:45:19 +0300

    Initial solution setup

---
+++ BackOfficeSystems.BrandDataImporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackOfficeSystems.BrandDataImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
